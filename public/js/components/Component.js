0970ac0ddfc769cb2817008195cae858defa07f1
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-01-29 11:46:40 +0100

    :sparkles: created Component component

--- public/js/components/Component.js
+++ public/js/components/Component.js
@@ -1,1 +1,9 @@
-// comentario jeje?
+export default class Component {
+  element;
+
+  constructor(parentElement, htmlTag, className) {
+    this.element = document.createElement(htmlTag);
+    this.element.className = className;
+    parentElement.append(this.element);
+  }
+}
